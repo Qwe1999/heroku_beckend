093e8f70bb8f9a7a4b92238bb75e9b3a2a820738
Qwe1999 <vladbanar5@gmail.com>
2020-01-14 18:00:15 +0300

    deleted config

--- src/main/java/com/softserve/academy/event/config/WebConfig.java
+++ src/main/java/com/softserve/academy/event/config/WebConfig.java
@@ -38,14 +38,14 @@ public class WebConfig implements WebMvcConfigurer {
         return new CommonsMultipartResolver();
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
